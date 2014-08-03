8d28e6807e917bf0af73b3edce7f1c25fce383e8
AshleighC <ashleigh.cushman@gmail.com>
2014-08-03 12:11:31 -0700

    Added js stuff.

--- Xavier/src/com/paea/xavier/XavierWebViewActivity.java
+++ Xavier/src/com/paea/xavier/XavierWebViewActivity.java
@@ -1,15 +1,33 @@
 package com.paea.xavier;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class XavierWebViewActivity extends Activity {
 
   private static final String URL = "http://www.elizabethylin.com/ychacks/";
 
+  private WebView webView;
+
+  private class WebClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+      webView.loadUrl(url);
+      return true;
+    }
+
+    @Override
+    public void onPageFinished(WebView webView, String url) {
+      webView.loadUrl("javascript:changeBackground()");
+    }
+  }
+
+  @SuppressLint("SetJavaScriptEnabled")
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -18,7 +36,9 @@ public class XavierWebViewActivity extends Activity {
     getWindow().getDecorView().setSystemUiVisibility(
         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
 
-    WebView webView = new WebView(this);
+    webView = new WebView(this);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.setWebViewClient(new WebClient());
     webView.loadUrl(URL);
 
     setContentView(webView);
